9baf28385a05f0276338e3018b10979a356deb2e
Manjunath Irukulla <imanjunad@gmail.com>
2024-08-17 19:58:23 +0530

    specific few pages on home

---
+++ project_bookeeda/lib/features/CartPage/cart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart Page'),
+      ),
+      body: Center(
+        child: Text('This is the Cart Page'),
+      ),
+    );
+  }
+}
