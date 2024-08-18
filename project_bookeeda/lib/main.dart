c73e10120daec13433222c722858d33f497b56fe
Manjunath Irukulla <imanjunad@gmail.com>
2024-08-18 21:51:21 +0530

    Design Homepage

--- project_bookeeda/lib/main.dart
+++ project_bookeeda/lib/main.dart
@@ -1,109 +1,145 @@
 import 'package:flutter/material.dart';
-import 'package:project_bookeeda/features/CartPage/cart_page.dart';
-import 'package:project_bookeeda/features/ProductPage/product_page.dart';
-import 'package:project_bookeeda/Onboarding/screen1.dart';
-import 'package:project_bookeeda/Onboarding/screen2.dart';
-import 'package:project_bookeeda/features/LandingPage/landing_page.dart';
-import 'package:project_bookeeda/features/PurchasePage/purchase_page.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart'; // Import the LandingPage
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LandingPage(),
-        '/product': (context) => ProductPage(),
-        '/cart': (context) => CartPage(),
-        '/purchase': (context) => PurchasePage(),
-      },
+      home: HomePage(),
     );
   }
 }
 
-class MyMainPage extends StatefulWidget {
-  const MyMainPage({super.key});
-
-  @override
-  State<MyMainPage> createState() => _MyMainPageState();
-}
-
-class _MyMainPageState extends State<MyMainPage> {
-  PageController pageController = PageController();
-  String buttonText = "Next";
-  int currentPageIndex = 0;
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          PageView(
-            controller: pageController,
-            onPageChanged: (index) {
-              currentPageIndex = index;
-              if (index == 1) {
-                buttonText = "Finish";
-              } else {
-                buttonText = "Next";
-              }
-              setState(() {});
-            },
-            children: [
-              Screen1(),
-              Screen2(),
-            ],
-          ),
-          Container(
-            alignment: const Alignment(0, 0.9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const SizedBox(),
-                GestureDetector(
-                  onTap: () {
-                    if (currentPageIndex == 1) {
-                      // Navigate to LandingPage
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => LandingPage()),
-                      );
-                    } else {
-                      pageController.nextPage(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.easeIn);
-                    }
-                  },
-                  child: Text("$buttonText"),
+      appBar: AppBar(
+        title: Text("Bookeeda"),
+        backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Top Banner
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/banner.jpg'), // Replace with your image
+                  fit: BoxFit.cover,
                 ),
-                SmoothPageIndicator(controller: pageController, count: 2),
-                currentPageIndex == 1
-                    ? SizedBox(width: 80)
-                    : GestureDetector(
-                        onTap: () {
-                          pageController.nextPage(
-                              duration: Duration(milliseconds: 500),
-                              curve: Curves.easeIn);
-                        },
-                        child: Text("Next"),
-                      ),
-              ],
+              ),
             ),
-          ),
+            SizedBox(height: 10),
+            // Categories Section
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                "Categories",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  CategoryItem(title: "Electronics"),
+                  CategoryItem(title: "Clothing"),
+                  CategoryItem(title: "Books"),
+                  CategoryItem(title: "Home & Kitchen"),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            // Products Grid
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                "Popular Products",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: 4, // Number of products
+              itemBuilder: (context, index) {
+                return ProductItem(
+                  title: "Product $index",
+                  price: "\$${(index + 1) * 20}",
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+
+  CategoryItem({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String price;
+
+  ProductItem({required this.title, required this.price});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.shopping_bag, size: 50, color: Colors.grey),
+          SizedBox(height: 10),
+          Text(title, style: TextStyle(fontSize: 16)),
+          SizedBox(height: 5),
+          Text(price,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
         ],
       ),
     );
