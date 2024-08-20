434216c5cfd70b36fb35f37b0bf2357e64a16b4b
Manjunath Irukulla <imanjunad@gmail.com>
2024-08-20 22:14:55 +0530

    Modifying Homepage

---
+++ project_bookeeda/lib/views/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Carousel extends StatelessWidget {
+  const Carousel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Bookeeda"),
+        ),
+      ),
+    );
+  }
+}
