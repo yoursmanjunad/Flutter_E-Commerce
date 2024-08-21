af066664e6f96adb3623e3200350cd713f788d8b
Manjunath Irukulla <imanjunad@gmail.com>
2024-08-21 20:50:48 +0530

    Adding Carousel FINAL

--- project_bookeeda/lib/main.dart
+++ project_bookeeda/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,10 +18,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Bookeeda"),
-          titleTextStyle:
-              TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold),
+          titleTextStyle: TextStyle(color: Colors.pinkAccent),
         ),
-        body: Center(),
         drawer: Drawer(
           child: ListView(
             children: [
@@ -44,6 +43,64 @@ class MyApp extends StatelessWidget {
               )
             ],
           ),
+        ),
+        body: Column(
+          children: [
+            // Carousel directly below the AppBar
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 600.0, // Set the desired height for the carousel
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                viewportFraction: 0.8,
+              ),
+              items: [
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    image: DecorationImage(
+                      image: AssetImage(
+                          "assets/book_images/breaktherules.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    image: DecorationImage(
+                      image: AssetImage(
+                          "assets/book_images/thehookupequation.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    image: DecorationImage(
+                      image:
+                          AssetImage("assets/book_images/bestfakefiance.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            // Other widgets below the carousel
+            Expanded(
+              child: Center(
+                child: Text("Content below the carousel"),
+              ),
+            ),
+          ],
         ),
       ),
     );
